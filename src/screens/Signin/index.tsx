2cb90b6178d50eb44c54bbc3c4013bac1fdf4ff5
Arjios <antonio.uab.ios.fisica@gmail.com>
2024-10-05 09:34:26 -0300

    Validate with Formik and Yup

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -3,34 +3,51 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { useNavigation } from "@react-navigation/native";
 import { AuthContext } from '../../context/Auth';
 
+import { Formik } from "formik";
+import * as Yup from 'yup';
+
 import { RootStackParamList } from "../../utils/types";
 
 import { Container, HeaderContainer, HeaderIcon, HeaderIconCheck, HeaderTitle } from "../../CommonStyles/styles";
 import { Feather } from "@expo/vector-icons";
-import { Alert, BackHandler } from "react-native";
+import { Alert, BackHandler, Text } from "react-native";
 import { ButtonLogin, InputContainer, InputIcon, MailInput, MainContainer, MainContent, MainTitle } from "./styles";
 
 
 type Props = NativeStackScreenProps<RootStackParamList>;
 
-export default function Signin() {
+type SigninProps = {
+    onBlur: (evt: any) => void
+}
+
+export default function Signin({ onBlur }: SigninProps) {
 
     const navigation = useNavigation<Props['navigation']>();
-    const {user} = useContext(AuthContext);
-    const [mail, setMail] = useState('')
-    const [password, setPassword] = useState('')
+    const { user } = useContext(AuthContext);
+    /*    const [mail, setMail] = useState('')
+        const [password, setPassword] = useState('') */
+
+    const loginSchema = Yup.object().shape({
+        mail: Yup.string()
+            .email(user.email)
+            .required('Email incorreto!'),
+        password: Yup.string()
+
+            .min(4, 'Senha deve conter no minimo 4 caracteres.')
+            .required('Senha não pode ser vazia.')
+    })
 
     function handleExitApp() {
         BackHandler.exitApp();
     };
 
-    function handleLoginApp() {
-        if (user.email !== mail || user.password !== password) {
+    function handleLoginApp(email: string, pass: string) {
+        if (user.email !== email || user.password !== pass) {
             Alert.alert('Usuario ou senha incorretas!')
             return
         };
         navigation.navigate('Home')
-        setPassword('')
+        /*        setPassword('') */
 
     };
 
@@ -48,42 +65,55 @@ export default function Signin() {
                 </HeaderIconCheck>
             </HeaderContainer>
             <MainContainer>
-                <MainContent>
-                    <InputContainer>
-                        <InputIcon>
-                            <Feather name='mail' size={32} color='#E6E6E6' />
-                        </InputIcon>
-                        <MailInput
-                            value={mail}
-                            onChangeText={(text: string) => setMail(text)}
-                            placeholder='Digite o email'
-                        />
-                    </InputContainer>
-                    <InputContainer>
-                        <InputIcon>
-                            <Feather name='unlock' size={32} color='#E6E6E6' />
-                        </InputIcon>
-                        <MailInput
-                            value={password}
-                            onChangeText={(pass: string) => setPassword(pass)}
-                            placeholder='Digite a senha'
-                        />
-                    </InputContainer>
+                <Formik
+                    initialValues={{ mail: '', password: '' }}
+                    validationSchema={loginSchema}
+                    onSubmit={(values) =>
+                        handleLoginApp(values.mail, values.password)
+                    }
+                >
+                    {({ handleSubmit, handleChange, handleBlur, values, errors, touched }) => (
+                        <MainContent>
+                            <InputContainer>
+                                <InputIcon>
+                                    <Feather name='mail' size={32} color='#E6E6E6' />
+                                </InputIcon>
+                                <MailInput
+                                    onChangeText={handleChange('mail')}
+                                    placeholder='Digite o email'
+                                    onBlur={onBlur}
+                                    value={values.mail}
 
-                </MainContent>
+                                />
+                            </InputContainer>
+                            {touched.mail && errors.mail && (
+                                <Text style={{ color: '#FF8477' }}>{'Email incorreto!'}</Text>
+                            )}
+                            <InputContainer>
+                                <InputIcon>
+                                    <Feather name='unlock' size={32} color='#E6E6E6' />
+                                </InputIcon>
+                                <MailInput
+                                    onChangeText={handleChange('password')}
+                                    placeholder='Digite a senha'
+                                    onBlur={onBlur}
+                                    value={values.password}
+                                />
 
-                <MainContent>
-                    <InputContainer>
-                        <ButtonLogin onPress={handleLoginApp}>
-                            <MainTitle >
-                                LOGIN
-                            </MainTitle>
-                        </ButtonLogin>
-                    </InputContainer>
-                </MainContent>
-
-
-
+                            </InputContainer>
+                            {touched.password && errors.password && (
+                                <Text style={{ color: '#FF8477' }}>{errors.password}</Text>
+                            )}
+                            <InputContainer>
+                                <ButtonLogin onPress={handleSubmit}>
+                                    <MainTitle >
+                                        LOGIN
+                                    </MainTitle>
+                                </ButtonLogin>
+                            </InputContainer>
+                        </MainContent>
+                    )}
+                </Formik>
             </MainContainer>
         </Container>
     );
